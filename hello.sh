cb806a5026866538faea45340a8c88aa89c9c97c
Ubuntu <ubuntu@ip-172-31-36-98.ec2.internal>
2024-05-03 12:03:13 +0000

    first day

---
+++ hello.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This is the first Script
+
+echo "Sumay: Hello Everyone"
+
+echo "Learner: Hi Sumay. How are you ?"
+echo "Sumay: Yes I am fine guys. What about you? "
+echo "Learner : we are cool!!! in this summer"
+
+
